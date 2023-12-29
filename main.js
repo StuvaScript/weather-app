c2ffc0f7a844c1cacc62c568b9fb3beb308ee4b9
Stu <stumac1000@yahoo.com>
2023-12-29 08:25:35 -0800

    Create a loop to get info for forecast

--- main.js
+++ main.js
@@ -942,15 +942,31 @@ async function getWeather() {
     );
     const data = await response.json();
 
-    //todo **`` Figure out how to run a loop through the three available days
-    // forecast day, min temp C
-    console.log(data.forecast.forecastday[0].day.mintemp_c);
-    // forecast day, min temp F
-    console.log(data.forecast.forecastday[0].day.mintemp_f);
-    // forecast day, min temp C
-    console.log(data.forecast.forecastday[0].day.maxtemp_c);
-    // forecast day, min temp F
-    console.log(data.forecast.forecastday[0].day.maxtemp_f);
+    console.group(`Current temps`);
+    console.log(data.current.temp_c + ' temp C');
+    console.log(data.current.temp_f + ' temp F');
+    console.log(data.current.condition.text + ' condition');
+    console.log(data.current.condition.icon.slice(-7) + ' icon number');
+    console.groupEnd();
+
+    data.forecast.forecastday.forEach((element) => {
+      console.group(`${element.date} day temps`);
+      console.log(element.day.mintemp_c + ' min temp C');
+      console.log(element.day.maxtemp_c + ' max temp C');
+      console.log(element.day.mintemp_f + ' min temp F');
+      console.log(element.day.maxtemp_f + ' max temp F');
+      console.log(element.day.condition.text + ' condition');
+      console.log(element.day.condition.icon.slice(-7) + ' icon number');
+      console.groupEnd();
+    });
+    // // forecast day, min temp C
+    // console.log(data.forecast.forecastday[0].day.mintemp_c);
+    // // forecast day, min temp F
+    // console.log(data.forecast.forecastday[0].day.mintemp_f);
+    // // forecast day, min temp C
+    // console.log(data.forecast.forecastday[0].day.maxtemp_c);
+    // // forecast day, min temp F
+    // console.log(data.forecast.forecastday[0].day.maxtemp_f);
     // update me
     console.log(data.forecast.forecastday);
     console.log(data.current);
@@ -960,14 +976,15 @@ async function getWeather() {
 }
 
 //* **`` Needed properties:
+// is_day
+// hourly? Might get pretty complicated...
+
 // condition: text
 // condition: icon
-// condition: code
 // maxtemp_f
 // maxtemp_c
 // mintemp_f
 // mintemp_c
-// is_day
 
 
 /***/ })
